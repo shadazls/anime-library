0abbcefde861822858b2a23a80bf3f55e57fd0cb
as105237 <as105237@S756P14>
2024-11-15 17:41:45 +0100

    Création du composant SearchInput + Travail sur l'utilisation du composant Autocomplete de NextUI

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,8 +54,8 @@ export default function RootLayout({
                 href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
                 title="nextui.org homepage"
               >
-                <span className="text-default-600">Powered by</span>
-                <p className="text-primary">NextUI</p>
+                <span className="text-default-600">Créé par</span>
+                <p className="text-primary">Shad AZUELOS & Marin LAFITTE</p>
               </Link>
             </footer>
           </div>
--- app/page.tsx
+++ app/page.tsx
@@ -7,49 +7,32 @@ import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
 
+import { SearchInput } from "@/components/SearchInput";
+import { Autocomplete, AutocompleteSection, AutocompleteItem } from "@nextui-org/autocomplete";
+import { animals } from "./fakeData";
+
 export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block max-w-xl text-center justify-center">
-        <span className={title()}>Make&nbsp;</span>
-        <span className={title({ color: "violet" })}>beautiful&nbsp;</span>
-        <br />
-        <span className={title()}>
-          websites regardless of your design experience.
-        </span>
-        <div className={subtitle({ class: "mt-4" })}>
-          Beautiful, fast and modern React UI library.
-        </div>
-      </div>
-
-      <div className="flex gap-3">
-        <Link
-          isExternal
-          className={buttonStyles({
-            color: "primary",
-            radius: "full",
-            variant: "shadow",
-          })}
-          href={siteConfig.links.docs}
+      <div className="mt-8 flex gap-4">
+        <SearchInput />
+        {/* <Autocomplete
+          label="Favorite Animal"
+          placeholder="Search an animal"
+          className="max-w-xs"
         >
-          Documentation
-        </Link>
-        <Link
-          isExternal
-          className={buttonStyles({ variant: "bordered", radius: "full" })}
-          href={siteConfig.links.github}
-        >
-          <GithubIcon size={20} />
-          GitHub
-        </Link>
-      </div>
+          {animals.map((animal) => (
+            <AutocompleteItem key={animal.value} value={animal.value}>
+              {animal.label}
+            </AutocompleteItem>
+          ))}
+        </Autocomplete> */}
 
-      <div className="mt-8">
-        <Snippet hideCopyButton hideSymbol variant="bordered">
+        {/* <Snippet hideCopyButton hideSymbol variant="bordered">
           <span>
             Get started by editing <Code color="primary">app/page.tsx</Code>
           </span>
-        </Snippet>
+        </Snippet> */}
       </div>
     </section>
   );
