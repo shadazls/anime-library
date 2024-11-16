44f5c5b600a65cb42a6997273dcefb903f94edf1
as105237 <shad.azuelos@etu.univ-cotedazur.fr>
2024-11-16 11:10:51 +0100

    Développement de cards d'animés + changement de background

--- app/fakeData.js
+++
@@ -1,1 +0,0 @@
-export const animals = {label: "Cat", value: "cat"}
--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,9 @@ import { GithubIcon } from "@/components/icons";
 import { SearchInput } from "@/components/SearchInput";
 import { Autocomplete, AutocompleteSection, AutocompleteItem } from "@nextui-org/autocomplete";
 import { Select, SelectItem } from "@nextui-org/select";
-import { animals } from "./fakeData";
+import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/card";
+import { Image } from "@nextui-org/image";
+import { Button, ButtonGroup } from "@nextui-org/button";
 
 export default function Home() {
   return (
@@ -66,31 +68,65 @@ export default function Home() {
           description="The type of the anime (e.g., TV series, movie, OVA, etc.)"
         ></Autocomplete>
         
-        <Autocomplete
+        {/* <Autocomplete
           label="Airing Status"
           labelPlacement="outside"
           placeholder="Any"
           className="max-w-xs"
           description="The status of the anime (e.g., Finished Airing, Currently Airing, etc.)"
-        ></Autocomplete>
+        ></Autocomplete> */}
 
-        {/* <Autocomplete
-          label="Favorite Animal"
-          placeholder="Search an animal"
-          className="max-w-xs"
-        >
-          {animals.map((animal) => (
-            <AutocompleteItem key={animal.value} value={animal.value}>
-              {animal.label}
-            </AutocompleteItem>
-          ))}
-        </Autocomplete> */}
-
-        {/* <Snippet hideCopyButton hideSymbol variant="bordered">
+        <Snippet hideCopyButton hideSymbol variant="bordered">
           <span>
             Get started by editing <Code color="primary">app/page.tsx</Code>
           </span>
-        </Snippet> */}
+        </Snippet>
+      </div>
+      <div className="mt-8 flex gap-4">
+        <Card
+          isBlurred
+          isFooterBlurred
+          radius="lg"
+          className="border-none"
+        >
+          <Image
+            width={225}
+            alt="Anime image"
+            src="https://cdn.myanimelist.net/images/anime/4/19644.jpg"
+          />
+          <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+            <p className="text-tiny text-white/80">ONE PIECE</p>
+            <Button className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg" size="sm">
+              EDIT
+            </Button>
+          </CardFooter>
+        </Card>
+        <Card
+          isPressable
+          isFooterBlurred
+          isHoverable
+          radius="lg"
+          className="border-none"
+        >
+          <Image
+            width={225}
+            alt="Anime image"
+            src="https://cdn.myanimelist.net/images/anime/1439/93480.jpg"
+          />
+          <CardFooter className="absolute bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between">
+            <div>
+              <p className="text-black text-medium">One Piece</p>
+            </div>
+            <Button className="text-tiny" color="primary" radius="full" size="sm">
+              EDIT
+            </Button>
+          </CardFooter>
+        </Card>
+        <Image
+          width={225}
+          alt="Anime image"
+          src="https://cdn.myanimelist.net/images/anime/7/20310.jpg"
+        />
       </div>
     </section>
   );
