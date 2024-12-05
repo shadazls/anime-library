dc4a1961a9c493d1e95005b80702d355dced12f0
as105237 <shad.azuelos@etu.unice.fr>
2024-12-05 18:57:22 +0100

    Amélioration du hook useAnimeStaff (suppression des doublons

--- app/hooks/useAnimeReviews.ts
+++ app/hooks/useAnimeReviews.ts
@@ -12,11 +12,6 @@ interface Review {
     body: string;
 }
 
-// interface Anime {
-//     _id: ObjectId;
-//     reviews?: Review[];
-// }
-
 const useAnimeReviews = (
     animeId: number | undefined,
     anime: Anime | null,
@@ -119,84 +114,3 @@ const useAnimeReviews = (
 };
 
 export default useAnimeReviews;
-
-// import { useEffect, useState } from 'react';
-
-// interface Review {
-//     id: number;
-//     user: {
-//         name: string;
-//         avatar: string;
-//     };
-//     score: number;
-//     summary: string;
-//     body: string;
-// }
-
-// const useAnimeReviews = (animeId: number | undefined, activeTab: string) => {
-//     const [reviews, setReviews] = useState<Review[] | null>(null);
-
-//     useEffect(() => {
-//         const fetchReviews = async () => {
-//             if (!animeId || activeTab !== 'reviews') return;
-
-//             const query = `
-//                 query ($id: Int) {
-//                     Media(id: $id) {
-//                         reviews {
-//                             nodes {
-//                                 id
-//                                 user {
-//                                     name
-//                                     avatar {
-//                                         large
-//                                     }
-//                                 }
-//                                 score
-//                                 summary
-//                                 body
-//                             }
-//                         }
-//                     }
-//                 }
-//             `;
-//             const variables = { id: animeId };
-
-//             try {
-//                 const response = await fetch('https://graphql.anilist.co', {
-//                     method: 'POST',
-//                     headers: {
-//                         'Content-Type': 'application/json',
-//                     },
-//                     body: JSON.stringify({ query, variables }),
-//                 });
-
-//                 const { data } = await response.json();
-
-//                 if (data?.Media?.reviews?.nodes) {
-//                     const fetchedReviews = data.Media.reviews.nodes.map(
-//                         (node: any) => ({
-//                             id: node.id,
-//                             user: {
-//                                 name: node.user.name,
-//                                 avatar: node.user.avatar.large,
-//                             },
-//                             score: node.score,
-//                             summary: node.summary,
-//                             body: node.body,
-//                         })
-//                     );
-//                     setReviews(fetchedReviews);
-//                 }
-//             } catch (error) {
-//                 console.error('Failed to fetch reviews:', error);
-//             }
-//         };
-
-//         fetchReviews();
-//     }, [animeId, activeTab]);
-
-//     return reviews;
-// };
-
-// export default useAnimeReviews;
--- app/hooks/useAnimeStaff.ts
+++ app/hooks/useAnimeStaff.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 
-interface StaffMember {
+interface Staff {
     id: number;
     name: {
         full: string;
@@ -12,12 +12,12 @@ interface StaffMember {
     role: string;
 }
 
-const useAnimeStaff = (animeId: number | undefined, activeTab: string) => {
-    const [staff, setStaff] = useState<StaffMember[] | null>(null);
+const useAnimeStaff = (animeId: number | undefined) => {
+    const [staff, setStaff] = useState<Staff[] | null>(null);
 
     useEffect(() => {
         const fetchStaff = async () => {
-            if (!animeId || activeTab !== 'staff') return;
+            if (!animeId) return;
 
             const query = `
                 query ($id: Int) {
@@ -54,16 +54,30 @@ const useAnimeStaff = (animeId: number | undefined, activeTab: string) => {
                 const { data } = await response.json();
 
                 if (data?.Media?.staff?.edges) {
-                    const formattedStaff = data.Media.staff.edges.map(
-                        (edge: any) => ({
-                            id: edge.node.id,
-                            name: edge.node.name,
-                            image: edge.node.image?.large,
-                            role: edge.role,
-                        })
-                    );
+                    // Organiser le staff pour supprimer les doublons
+                    const staffMap: Record<number, Staff> = {};
 
-                    setStaff(formattedStaff);
+                    data.Media.staff.edges.forEach((edge: any) => {
+                        const { id, name, image } = edge.node;
+                        const { role } = edge;
+
+                        // Si le membre du staff existe déjà dans le tableau, ajouter le rôle
+                        if (staffMap[id]) {
+                            staffMap[id].role = `${staffMap[id].role}, ${role}`;
+                        } else {
+                            // Si le membre du staff n'existe pas encore, l'ajouter
+                            staffMap[id] = {
+                                id,
+                                name,
+                                image: image.large,
+                                role,
+                            };
+                        }
+                    });
+
+                    // Convertir le staffMap en tableau et mettre à jour l'état
+                    const uniqueStaff = Object.values(staffMap);
+                    setStaff(uniqueStaff);
                 }
             } catch (error) {
                 console.error('Failed to fetch staff:', error);
@@ -71,9 +85,90 @@ const useAnimeStaff = (animeId: number | undefined, activeTab: string) => {
         };
 
         fetchStaff();
-    }, [animeId, activeTab]);
+    }, [animeId]);
 
     return staff;
 };
 
 export default useAnimeStaff;
+
+// import { useEffect, useState } from 'react';
+
+// interface StaffMember {
+//     id: number;
+//     name: {
+//         full: string;
+//         native?: string;
+//     };
+//     image: {
+//         large: string;
+//     };
+//     role: string;
+// }
+
+// const useAnimeStaff = (animeId: number | undefined, activeTab: string) => {
+//     const [staff, setStaff] = useState<StaffMember[] | null>(null);
+
+//     useEffect(() => {
+//         const fetchStaff = async () => {
+//             if (!animeId || activeTab !== 'staff') return;
+
+//             const query = `
+//                 query ($id: Int) {
+//                     Media(id: $id) {
+//                         staff {
+//                             edges {
+//                                 node {
+//                                     id
+//                                     name {
+//                                         full
+//                                         native
+//                                     }
+//                                     image {
+//                                         large
+//                                     }
+//                                 }
+//                                 role
+//                             }
+//                         }
+//                     }
+//                 }
+//             `;
+//             const variables = { id: animeId };
+
+//             try {
+//                 const response = await fetch('https://graphql.anilist.co', {
+//                     method: 'POST',
+//                     headers: {
+//                         'Content-Type': 'application/json',
+//                     },
+//                     body: JSON.stringify({ query, variables }),
+//                 });
+
+//                 const { data } = await response.json();
+//                 console.log(data);
+
+//                 if (data?.Media?.staff?.edges) {
+//                     const formattedStaff = data.Media.staff.edges.map(
+//                         (edge: any) => ({
+//                             id: edge.node.id,
+//                             name: edge.node.name,
+//                             image: edge.node.image?.large,
+//                             role: edge.role,
+//                         })
+//                     );
+
+//                     setStaff(formattedStaff);
+//                 }
+//             } catch (error) {
+//                 console.error('Failed to fetch staff:', error);
+//             }
+//         };
+
+//         fetchStaff();
+//     }, [animeId, activeTab]);
+
+//     return staff;
+// };
+
+// export default useAnimeStaff;
