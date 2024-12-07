fffe8f1ea583f909d9df29716fd60cfea9c3a9e2
as105237 <shad.azuelos@etu.unice.fr>
2024-12-07 13:05:49 +0100

    Formattage des avis

--- components/ReviewItem.tsx
+++ components/ReviewItem.tsx
@@ -1,6 +1,7 @@
 import { Review } from '@/types';
 import { Avatar } from '@nextui-org/avatar';
 import { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
 
 const ReviewItem = ({ review }: { review: Review }) => {
     const [isExpanded, setIsExpanded] = useState(false);
@@ -10,45 +11,43 @@ const ReviewItem = ({ review }: { review: Review }) => {
 
     const renderBody = () => {
         if (isExpanded || review.body.length <= maxBodyLength) {
-            return review.body;
+            return <ReactMarkdown>{review.body}</ReactMarkdown>;
         }
 
-        return `${review.body.slice(0, maxBodyLength)}... `;
+        // Tronquer le contenu et le rendre sous forme de Markdown
+        const truncatedBody = review.body.slice(0, maxBodyLength);
+        return (
+            <>
+                <ReactMarkdown>{`${truncatedBody}...`}</ReactMarkdown>
+                <button
+                    onClick={toggleBody}
+                    className="text-blue-400 hover:underline"
+                >
+                    {isExpanded ? 'moins' : 'plus'}
+                </button>
+            </>
+        );
     };
 
     return (
-        <>
-            <div className="flex gap-4 mb-16">
-                <div className="flex-shrink-0">
-                    <Avatar
-                        src={review.user.avatar}
-                        showFallback
-                        name={review.user.name}
-                        size="lg"
-                    />
-                </div>
-                <div className="flex flex-col gap-2">
-                    <div className="flex gap-4">
-                        <h4 className="font-bold">{review.user.name}</h4>
-                        <p className="text-gray-500">
-                            Score: {review.score}/100
-                        </p>
-                    </div>
-                    <h3 className="font-semibold mb-2">{review.summary}</h3>
-                    <p className="text-gray-400">
-                        {renderBody()}
-                        {review.body.length > maxBodyLength && (
-                            <button
-                                onClick={toggleBody}
-                                className="text-blue-400 hover:underline"
-                            >
-                                {isExpanded ? 'moins' : 'plus'}
-                            </button>
-                        )}
-                    </p>
-                </div>
+        <div className="flex gap-4 mb-16">
+            <div className="flex-shrink-0">
+                <Avatar
+                    src={review.user.avatar}
+                    showFallback
+                    name={review.user.name}
+                    size="lg"
+                />
             </div>
-        </>
+            <div className="flex flex-col gap-2">
+                <div className="flex gap-4">
+                    <h4 className="font-bold">{review.user.name}</h4>
+                    <p className="text-gray-500">Score: {review.score}/100</p>
+                </div>
+                <h3 className="font-semibold mb-2">{review.summary}</h3>
+                <div className="text-gray-400">{renderBody()}</div>
+            </div>
+        </div>
     );
 };
 
